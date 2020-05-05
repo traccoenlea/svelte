6ef08107677e6aa74a0fd64a4135c1595f84d779
Fautrat Juliette <juliette.fautrat@gmail.com>
2020-05-05 12:02:53 +0200

    objetctifst

--- src/Objectifs.svelte
+++ src/Objectifs.svelte
@@ -1,58 +1,74 @@
 <div class="fond">
 
-	<div class="texte"> 
+    <div class="groupe">
+    	<div class="groupe1"> 
+            <h1> OBJECTIFS & COMPÉTENCES </h1> <h1 class="orange"> DE LA FORMATION</h1>
+            <p> La Licence Professionnelle Métiers du numérique : </p>
+            <p> -  de concevoir et de réaliser des sites web aux fonctionnalités multiples </p>
+            <p> - De concevoir et de créer des contenus appropriés à chaque site et répondant à des besoins spécifiques (écrits, visuels, infographiques). </p>
+        </div>
 
-        <h1> OBJECTIFS & COMPÉTENCES DE LA FORMATION</h1><br>
-        <p> - De concevoir et de réaliser des sites web aux fonctionnalités multiples.
-		<p> - De concevoir et de créer des contenus appropriés à chaque site et répondant à des besoins spécifiques (écrits, visuels, infographiques). </p>
-		<p>	  Elle leur permet également :</p>
-		<p> - De maîtriser les connaissances et les compétences indispensables à la conception d’un projet dans le domaine du web.</p>
-		<p> - De maîtriser les connaissances et les compétences indispensables à la conception d’un projet dans le domaine du web.</p>
-		<p> - D’acquérir une méthodologie rigoureuse de la gestion d’un projet.</p>
-		<p> - D’acquérir des compétences en communication orale et écrite à des fins argumentatives (présentation de projets, prise de décision, expression de choix etc.)</p>
+        <div class="groupe2"> 
 
-	</div>
+    		<p>	  Elle leur permet également :</p>
+    		<p> - De maîtriser les connaissances et les compétences indispensables à la conception d’un projet dans le domaine du web.</p>
+    		<p> - De maîtriser les connaissances et les compétences indispensables à la conception d’un projet dans le domaine du web.</p>
+    		<p> - D’acquérir une méthodologie rigoureuse de la gestion d’un projet.</p>
+    		<p> - D’acquérir des compétences en communication orale et écrite à des fins argumentatives (présentation de projets, prise de décision, expression de choix etc.)</p>
 
+    	</div>
+    </div>
 </div>
 
 
 <style>
 	
 	.fond {
-	    height :100vh;
 	    background: url('/img/groupe.png') no-repeat center fixed; 
 	    background-size: cover;
 	    height: 100vh;
 	    display: flex;
-    	justify-content: left;
-    	padding-left: 150px;
+        justify-content: center;
     	align-items: center;
     }
 
-    .texte {
+    .groupe{
+        display: flex;
+        height: 300px;
 
-		position:absolute; 
-  		top:40%; 
- 		height:10em; 
-  		margin-top:-5em; 
-  		width:100%;
+    }
 
-	}
+
+    .groupe1 {
+
+        width: 50%;
+        margin-left: 150px;
+        border-right: 1px solid white;
+    }
+
+    .groupe2 {
+        width: 50%;
+        margin-right: 150px;
+    }
 
     h1 {
         color : white;
         font-family: 'Raleway-Bold', sans-serif;
-        font-size: 32px;
-		padding:0px 20px;
+        font-size: 30px;
+    }
+
+    .orange {
+
+        color: #FF7F00;
     }
 
     p {
         
         color : white;
-        font-family: 'Noto Sans', sans-serif; 
-        font-size:14px;
-  		padding:0px 20px;
-        margin: 20px;
+        font-family: 'Raleway-Regular', sans-serif;
+        font-size: 15px;
+        padding: 10px;
 
     }
+
 </style>
