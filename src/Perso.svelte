1f10dfe4e9b515ec70ef8fc31b83b7f576f41bc3
Fautrat Juliette <juliette.fautrat@gmail.com>
2020-05-06 19:33:46 +0200

    design fini Juju

--- src/Perso.svelte
+++ src/Perso.svelte
@@ -29,8 +29,8 @@
     let color = 'white';
     let textColor = 'black';
     let fillColor = 'black';
-    // let divColor = '#FF7F00';
-    let divColor = 'aqua';
+    let divColor = '#FF7F00';
+
     let fonts = ['Times New Roman', 'Georgia', 'Arial', 'Verdana', 'Monospace', 'Courier New', 'Lucida Console'];
     var font = 'Raleway';
     function changeFont() {
@@ -78,106 +78,112 @@
                 </div>
             </div>
 
-            <Icon data={flag} id="iconChange"  style="fill : {fillColor}" scale="2"/>
+            <!--<Icon data={flag} id="iconChange"  style="fill : {fillColor}" scale="2"/> -->
+        </div>
 
-
-
+        <div class="toggle">
+            <div on:click={openNav} class="color">Modifier la page</div>
         </div>
     </div>
 
-    <div class="toggle">
-        <div on:click={openNav} class="color">Modifier la page</div>
-    </div>
+
 
 
     <div id="sideForm" class="sideForm">
+
         <a href="javascript:void(0)" class="closebtn" on:click={closeNav}>&times;</a>
         <div id="form">
-            <label for="changeFormation">Titre de la formation :</label>
-            <input type="text" id="changeFormation" bind:value={nomFormation}/>
+            <!--<label class="what" for="changeFormation">Nom de la formation : </label> -->
+            <input class="nom" type="text" id="changeFormation" bind:value={nomFormation}/>
 
             <div class="langue">
                 <label for="changeLieu">
-                    <img src={location} alt="icone de lieu de formation">
+                    <!--<img src={location} alt="icone de lieu de formation">-->
                 </label>
-                <input type="text" id="changeLieu" bind:value={lieuFormation}/>
+                <input class="nom" type="text" id="changeLieu" bind:value={lieuFormation}/>
             </div>
 
             <div class="langue">
                 <label for="changeLangueCours">
-                    <img src={langue} alt="icone de la langue utilisée">
+                    <!--<img src={langue} alt="icone de la langue utilisée">-->
                 </label>
-                <input type="text" id="changeLangueCours" bind:value={langueCours}/>
+                <input class="nom" type="text" id="changeLangueCours" bind:value={langueCours}/>
             </div>
 
             <div class="langue">
                 <label for="changeModalites">
-                    <img src={education} alt="icone des modalités">
+                    <!--<img src={education} alt="icone des modalités">-->
                 </label>
-                <input type="text" id="changeModalites" bind:value={modalites}/>
+                <input class="nom" type="text" id="changeModalites" bind:value={modalites}/>
             </div>
 
             <div class="langue">
                 <label for="changeAdmissibilite">
-                    <img src={doc} alt="icone de niveau de formation nécessaire">
+                   <!-- <img src={doc} alt="icone de niveau de formation nécessaire">-->
                 </label>
-                <input type="text" id="changeAdmissibilite" bind:value={admissible}/>
+                <input class="nom" type="text" id="changeAdmissibilite" bind:value={admissible}/>
             </div>
 
             <div class="langue">
                 <label for="changeSortie">
-                    <img src={diplomas} alt="icone de niveau obtenu">
+                    <!--<img src={diplomas} alt="icone de niveau obtenu">-->
                 </label>
-                <input type="text" id="changeSortie" bind:value={sortie}/>
+                <input class="nom" type="text" id="changeSortie" bind:value={sortie}/>
             </div>
 
             <div class="heures">
-                <label for="changeHeures">Nombre d'heures d'enseignements : </label>
-                <input type="text" id="changeHeures" bind:value={heures}/>
+               <!-- <label class="what" for="changeHeures">Nombre d'heures d'enseignements : </label> -->
+                <input class="nom" type="text" id="changeHeures" bind:value={heures}/>
             </div>
 
         </div>
 
-        <hr>
-
         <div class="colorsToChange">
 
-            <label for="bgColor">Changement couleur background :</label>
-            <input id="bgColor" type="color" bind:value={color} style="height: 50px;">
+        <div class="si">
 
-            <br/><br/>
+            <div class="lolo">
+                <label class="what" for="bgColor">Fond :</label>
+                <input class="coloriage" id="bgColor" type="color" bind:value={color} style="height: 50px;">
+            </div>
 
-
-            <label for="textColor">Changement couleur du texte :</label>
-            <input id="textColor" type="color" bind:value={textColor} style="height: 50px;">
-
-            <br/><br/>
-
-            <label for="fillColor">Changement couleur des icones :</label>
-            <input id="fillColor" type="color" bind:value={fillColor} style="height: 50px;">
-
-            <br/><br/>
-
-            <label for="divColor">Changement couleur du trait :</label>
-            <input id="divColor" type="color" bind:value={divColor} style="height: 50px;">
-
-            <br/><br/>
+  
 
 
-            <label for="fontChange">Changement de font :</label>
+            <div class="lolo">
+                <label class="what" for="textColor">Texte :</label>
+                <input class="coloriage" id="textColor" type="color" bind:value={textColor} style="height: 50px;">
+            </div>
 
-            <select id="fontChange">
-                {#each fonts as font}
-                    <option>{font}</option>
-                {/each}
-            </select>
+     
 
-            <br/>
+            <div class="lolo">
+                <label class="what" for="fillColor">Icônes :</label>
+                <input class="coloriage" id="fillColor" type="color" bind:value={fillColor} style="height: 50px;">
+            </div>
 
-            <button on:click={changeFont}>
-                On change de police !
-            </button>
+     
 
+            <div class="lolo">
+                <label class="what" for="divColor">Trait :</label>
+                <input class="coloriage" id="divColor" type="color" bind:value={divColor} style="height: 50px;">
+            </div>
+    </div>
+
+             <div class="lulu">
+                <!--<label class="what" for="fontChange">Fond :</label>-->
+
+                <select class="ok" id="fontChange">
+                    {#each fonts as font}
+                        <option>{font}</option>
+                    {/each}
+                </select>
+
+
+                <button class="ok2" on:click={changeFont}>
+                    On change de police !
+                </button>
+             </div>
         </div>
     </div>
 
@@ -185,19 +191,108 @@
 
 
 <style>
+
+    .ok {
+        outline: none;
+        border : none;
+        background-color: rgba(255, 255, 255, 0.10);
+        text-align: right;
+        padding: 20px;
+        font-weight: 300;
+        color: white;
+        font-size: 12px;
+        border-radius: 6px;
+        text-align: center;
+        font-family: 'Raleway-Light', sans-serif;
+        cursor: pointer;
+        width: 35%;
+        margin-right: 20px;
+
+    }
+
+    .ok2 {
+        outline: none;
+        border : none;
+        background-color: rgba(255, 255, 255, 0.10);
+        color: white;
+        font-size: 12px;
+        border-radius: 6px;
+        text-align: center;
+        font-family: 'Raleway-Light', sans-serif;
+        cursor: pointer;
+        padding: 10px;
+        width: 150px;
+
+    }
+
+    .lulu {
+
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 85%;
+        margin-top: 35px;
+
+    }
+
+    .si {
+
+        display: flex;
+        align-items: center;
+        width: 85%;
+        justify-content: center;
+
+    }
+
+    .what {
+
+        font-size: 12px;
+        font-family: 'Raleway-Bold', sans-serif;
+        color: rgba(255, 255, 255, 0.50);
+        text-transform : uppercase;
+        margin-left: 12px;
+        margin-top: 12px;
+    }
+
+    .nom {
+
+    display: flex;
+    width: 330px;
+    border: none;
+    background-color: rgba(255, 255, 255, 0.10);
+    font-family: 'Raleway-Bold', sans-serif;
+    color: white;
+    padding-right: 25px;
+    padding-left: 25px;
+    padding-top: 15px;
+    padding-bottom: 15px;
+    border-radius: 6px;
+    font-size: 14px;
+    margin: 10px 0px;
+    outline: none;
+    height: 15px;
+
+    }
+
+
+    
     #iconChange {
         fill: var(--fill-color);
         background: red;
     }
+
     #ligne {
         border-top : 5px solid var(--div-color);
     }
+
     /*raleway to remove*/
+
     h2 {
         font-weight: 600;
         font-size: 20px;
         padding-bottom: 20px;
     }
+
     p {
         font-weight: 300;
         font-size: 15px;
@@ -211,53 +306,71 @@
         color: white;
         font-size: 15px;
         border-radius: 2px;
+        text-align: center;
+        font-family: 'Raleway-Light', sans-serif;
+        cursor: pointer;
     }
-    /*font & color*/
+
+/*font & color*/
+
     #bgChange {
         background: var(--bg-color);
         padding: 30px;
     }
+
     #txtChange {
         font-weight: bold;
         margin: 20px;
         font-size: 20px;
         color: var(--text-color);
     }
+
     #fontHere {
         font-family: var(--font-family);
         font-size: 30px;
     }
+
     .sideForm {
-        height: 100%;
-        width: 500px; /* NE PAS CHANGER LA WIDTH !!  SE CHANGE EN JS*/
+        height: 100vh;
+        width: 30%; /* NE PAS CHANGER LA WIDTH !!  SE CHANGE EN JS*/
         position: fixed;
         z-index: 1;
         top: 0;
         right: 0;
-        background-color: #FF7F00;
+        background-color: rgba(1, 1, 1, 0.80);
         overflow-x: hidden;
         transition: 0.5s;
         padding: 20px;
         padding-top: 60px;
+        box-shadow: 15px 45px 45px black;
     }
+
+/* CROIX */
+
     .sideForm a {
         padding: 20px;
         text-decoration: none;
         font-size: 50px;
-        color: purple;
+        color: white;
         display: block;
         transition: 0.3s;
+        
     }
     .sideForm a:hover {
-        color: red;
+        color: white;
     }
+
+
     .sideForm .closebtn {
         position: absolute;
         top: 0;
         right: 25px;
-        font-size: 36px;
+        font-size: 40px;
         margin-left: 50px;
     }
+
+/* Rectangle gauche */
+
     .fond {
         height: 100vh;
         background: url('/img/fond.png') no-repeat center fixed;
@@ -290,8 +403,44 @@
         flex-direction: column;
     }
     .ligne {
-        /*border-top: 5px solid #EE8B29;*/
         height: 30px;
         width: 70px;
+    }
+
+
+
+    .colorsToChange {
+
+        display: flex;
+        flex-wrap: wrap;
+        height: 150px;
+        width: 100%;
+        padding-top: 10px;
+        padding-bottom: 10px;
+    }
+
+
+    .coloriage {
+
+        border : none;
+        display: flex;
+        flex-direction: wrap;
+        width: 45px;
+        border: none;
+        border-radius: 6px;
+        outline: none;
+        background-color: rgba(255, 255, 255, 0.10);
+        height: 10px;
+        padding: 10px;
+
+    }
+
+    .lolo {
+
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+
     }
 </style>
